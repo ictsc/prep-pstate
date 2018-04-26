89f9f261881572030cc8a0be35a0e03f14e7d3ad
tar-xzvff <jv120228m@gmail.com>
2018-04-26 18:05:32 +0900

    問題環境の情報を格納するモデルを追加

--- ictsc_prep_school/pstate/models/__init__.py
+++ ictsc_prep_school/pstate/models/__init__.py
@@ -18,3 +18,29 @@ class Problem(TemplateModel):
     is_enabled = models.BooleanField("公開フラグ", default=False)
     #   TODO    :   terraform-managerのコードをpstateに持ってきたら下のコメントアウトを外す.
     # terraform_file_id = models.ForeignKey('TerraformFile')
+
+
+class ProblemEnvironment(TemplateModel):
+    STATE_CHOICES = (
+        ('IN_PREPARATION', 'IN_PREPARATION'),
+        ('READY', 'READY'),
+        ('IN_PROGRESS', 'IN_PROGRESS'),
+        ('FINISH', 'FINISH'),
+        ('WAITING_FOR_SCORING', 'WAITING_FOR_SCORING'),
+        ('WAITING_FOR_DELETE', 'WAITING_FOR_DELETE'),
+        ('DELETED', 'DELETED'),
+
+    )
+
+    vnc_server_ipv4_address = models.GenericIPAddressField(protocol='IPv4', blank=True, null=True)
+    vnc_server_port = models.CharField(max_length=5, blank=True, null=True)
+    vnc_server_username = models.CharField(max_length=50, blank=True, null=True)
+    vnc_server_password = models.CharField(max_length=50, blank=True, null=True)
+    is_enabled = models.BooleanField("有効フラグ", default=False)
+    state = models.CharField(choices=STATE_CHOICES, default='IN_PREPARATION')
+    #   TODO    :   モデルの設計の方針が決まったら下のコメントアウトを外す.
+    # team = models.ForeignKey(Team)
+    # participant = models.ForeignKey(Participant)
+    #   TODO    :   terraform-managerのコードをpstateに持ってきたら下のコメントアウトを外す.
+    # environment = models.ForeignKey(Environment)
+    problem = models.ForeignKey(Problem)

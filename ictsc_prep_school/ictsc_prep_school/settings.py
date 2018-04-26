1791abd495a57cb6dafa094f57846d2eec132f3f
tar-xzvff <jv120228m@gmail.com>
2018-04-26 17:15:22 +0900

    adminlteとpstateアプリケーションの追加

--- ictsc_prep_school/ictsc_prep_school/settings.py
+++ ictsc_prep_school/ictsc_prep_school/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_adminlte',
+    'django_adminlte_theme',
+    'pstate',
 ]
 
 MIDDLEWARE = [

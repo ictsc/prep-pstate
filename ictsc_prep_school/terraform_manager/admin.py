260a222c4150fb8b3e1974c43ece3e004a069541
tar-xzvff <jv120228m@gmail.com>
2018-05-02 19:39:15 +0900

    terraform-managerのadmin.pyを移植

--- ictsc_prep_school/terraform_manager/admin.py
+++ ictsc_prep_school/terraform_manager/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from terraform_manager.models import Log, Variable, Environment, TerraformFile, ShellScript, Provider, Attribute
+
+admin.site.register(Log)
+admin.site.register(Variable)
+admin.site.register(Environment)
+admin.site.register(TerraformFile)
+admin.site.register(ShellScript)
+admin.site.register(Provider)
+admin.site.register(Attribute)

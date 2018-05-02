239939dbe624f50558d61a7d077c3557ab626ccc
tar-xzvff <jv120228m@gmail.com>
2018-05-02 19:48:51 +0900

    terraform-managerのsettingsにアプリケーションを追加

--- ictsc_prep_school/ictsc_prep_school/settings.py
+++ ictsc_prep_school/ictsc_prep_school/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pstate',
+    'terraform_manager',
     'django_adminlte',
     'django_adminlte_theme',
 ]

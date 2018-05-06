5ddbfe4f0dfae0b8cfcd23d7be8b2e6cf5971746
tar-xzvff <jv120228m@gmail.com>
2018-05-06 11:15:51 +0900

    詳細と編集と削除画面へのURLルーティング設定を追加

--- ictsc_prep_school/pstate/urls.py
+++ ictsc_prep_school/pstate/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from pstate import views
 
-from pstate.views import ParticipantListView, ParticipantCreateView, TeamListView, TeamCreateView
+from pstate.views import ParticipantListView, ParticipantCreateView, TeamListView, TeamCreateView, TeamDetailView, TeamUpdateView, TeamDeleteView
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -11,5 +11,8 @@ urlpatterns = [
     url(r'^participants/$', ParticipantListView.as_view(), name='participant-list'),
     url(r'^participants/add/$', ParticipantCreateView.as_view(), name='participant-add'),
     url(r'^teams/$', TeamListView.as_view(), name='team-list'),
+    url(r'^teams/(?P<pk>[0-9]+)/$', TeamDetailView.as_view(), name='team-detail'),
     url(r'^teams/add/$', TeamCreateView.as_view(), name='team-add'),
+    url(r'^teams/(?P<pk>[0-9]+)/edit/$', TeamUpdateView.as_view(), name='team-edit'),
+    url(r'^teams/(?P<pk>[0-9]+)/delete/$', TeamDeleteView.as_view(), name='team-delete'),
 ]

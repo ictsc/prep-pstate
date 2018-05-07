40a954e4d782608d493012ec4c33fa3e118fc96c
tar-xzvff <jv120228m@gmail.com>
2018-05-07 16:06:13 +0900

    ProviderFormにcss classを追加

--- ictsc_prep_school/pstate/forms/add_provider.py
+++ ictsc_prep_school/pstate/forms/add_provider.py
@@ -7,4 +7,9 @@ class ProviderForm(forms.ModelForm):
 
     class Meta:
         model = Provider
-        fields = ('__all__')
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': "form-control"}),
+            'provider_name': forms.TextInput(attrs={'class': "form-control"}),
+            # 'attribute': forms.ModelMultipleChoiceField(queryset=None),
+        }

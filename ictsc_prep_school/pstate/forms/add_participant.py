619a6d6a26988d3387b6c828fea53b92c18f0317
tar-xzvff <jv120228m@gmail.com>
2018-05-08 12:48:34 +0900

    Participantの追加画面で出力する内容を追加

--- ictsc_prep_school/pstate/forms/add_participant.py
+++ ictsc_prep_school/pstate/forms/add_participant.py
@@ -7,4 +7,21 @@ class ParticipantForm(forms.ModelForm):
 
     class Meta:
         model = Participant
-        fields = ('__all__')
+        fields = ['first_name', 'last_name', 'username', 'password', 'email', 'assign_team']
+        exclude = ['last_login', 'date_joined', 'is_superuser', 'groups', 'user_permissions',
+                   'is_staff']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': "form-control"}),
+            'last_name': forms.TextInput(attrs={'class': "form-control"}),
+            'password': forms.PasswordInput(attrs={'class': "form-control"}),
+            'username': forms.TextInput(attrs={'class': "form-control"}),
+            'email': forms.EmailInput(attrs={'class': "form-control"}),
+        }
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        participant = super(ParticipantForm, self).save(commit=False)
+        participant.set_password(self.cleaned_data["password"])
+        if commit:
+            participant.save()
+        return participant

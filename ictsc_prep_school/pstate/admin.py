f05319f299e0437c6bc9733601a049c0685c6753
tar-xzvff <jv120228m@gmail.com>
2018-05-08 12:52:17 +0900

    UserモデルをDjango adminから編集できるように変更

--- ictsc_prep_school/pstate/admin.py
+++ ictsc_prep_school/pstate/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from pstate.models import Team, Participant, Grade, Problem, ProblemEnvironment
+from pstate.models import Team, Participant, Grade, Problem, ProblemEnvironment, User
 
+admin.site.register(User)
 admin.site.register(Team)
 admin.site.register(Participant)
 admin.site.register(Grade)

30322c23e6328dc04d81fab8271c4121ccbcd01a
tar-xzvff <jv120228m@gmail.com>
2018-05-08 12:08:58 +0900

    file add

---
+++ ictsc_prep_school/ictsc_prep_school/settings/pstate/production.py
@@ -0,0 +1,17 @@
+from ictsc_prep_school.settings.pstate.base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'terraform_manager',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+BROKER_URL = 'redis://:6379/0'
+CELERY_RESULT_BACKEND = 'redis://:6379/0'
+
+DEBUG = False

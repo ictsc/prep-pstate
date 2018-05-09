9cb5f97208c50af1260e5d912151e23c30ca5f6b
tar-xzvff <jv120228m@gmail.com>
2018-05-09 13:36:41 +0900

    TeamとParticipantの編集画面のFormを追加

--- ictsc_prep_school/pstate/forms/add_participant.py
+++ ictsc_prep_school/pstate/forms/add_participant.py
@@ -25,3 +25,24 @@ class ParticipantForm(forms.ModelForm):
         if commit:
             participant.save()
         return participant
+
+
+class ParticipantUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Participant
+        fields = ['first_name', 'last_name', 'username', 'email', 'assign_team']
+        exclude = ['password', 'last_login', 'date_joined', 'is_superuser', 'groups', 'user_permissions',
+                   'is_staff']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': "form-control"}),
+            'last_name': forms.TextInput(attrs={'class': "form-control"}),
+            'username': forms.TextInput(attrs={'class': "form-control"}),
+            'email': forms.EmailInput(attrs={'class': "form-control"}),
+        }
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        participant = super(ParticipantUpdateForm, self).save(commit=False)
+        if commit:
+            participant.save()
+        return participant

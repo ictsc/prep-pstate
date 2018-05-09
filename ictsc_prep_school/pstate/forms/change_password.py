50bda7ccc006fde307b4d5d8c5825094422df327
tar-xzvff <jv120228m@gmail.com>
2018-05-09 13:47:59 +0900

    add comment out

--- ictsc_prep_school/pstate/forms/change_password.py
+++ ictsc_prep_school/pstate/forms/change_password.py
@@ -3,5 +3,6 @@ from django.contrib.auth.forms import PasswordChangeForm
 
 class NoOlbPasswordCheckPasswordChangeForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
+        # user = kwargs['user']
         super(NoOlbPasswordCheckPasswordChangeForm, self).__init__(*args, **kwargs)
         del self.fields['old_password']

48ccac5acc207b5c5e41667c9a56a7735bfb1bf3
tar-xzvff <jv120228m@gmail.com>
2018-05-09 15:09:27 +0900

    登録画面へのURLルーティング設定を追加

--- ictsc_prep_school/ictsc_prep_school/urls.py
+++ ictsc_prep_school/ictsc_prep_school/urls.py
@@ -1,7 +1,12 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+
+from pstate.views import TeamRegisterView, ParticipantRegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('manage/', include('pstate.urls')),
+    url('register/team/$', TeamRegisterView.as_view(), name='team-register'),
+    url('register/participant/$', ParticipantRegisterView.as_view(), name='participant-register'),
 ]

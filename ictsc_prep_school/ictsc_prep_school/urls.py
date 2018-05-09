b3efbda6763c30833fdac3a61f0a6c7bfefc6eaa
tar-xzvff <jv120228m@gmail.com>
2018-05-09 15:16:15 +0900

    参加者画面のURLルーティング設定を追加

--- ictsc_prep_school/ictsc_prep_school/urls.py
+++ ictsc_prep_school/ictsc_prep_school/urls.py
@@ -7,6 +7,7 @@ from pstate.views import TeamRegisterView, ParticipantRegisterView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('manage/', include('pstate.urls')),
+    path('user/', include('pstate.user_page_urls')),
     url('register/team/$', TeamRegisterView.as_view(), name='team-register'),
     url('register/participant/$', ParticipantRegisterView.as_view(), name='participant-register'),
 ]

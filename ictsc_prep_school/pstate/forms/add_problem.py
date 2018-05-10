0e1b91df2f784ef08d912ffd183b102d70bc2d92
tar-xzvff <jv120228m@gmail.com>
2018-05-10 18:27:27 +0900

    問題追加と編集のフォームの内容を追加更新

--- ictsc_prep_school/pstate/forms/add_problem.py
+++ ictsc_prep_school/pstate/forms/add_problem.py
@@ -7,8 +7,25 @@ class ProblemForm(forms.ModelForm):
 
     class Meta:
         model = Problem
-        fields = ('name', 'display_name', 'description', 'start_date', 'end_date', 'is_enabled', )
+        fields = ('name', 'display_name', 'description', 'is_enabled', )
+        # fields = ('name', 'display_name', 'description', 'start_date', 'end_date', 'is_enabled', )
+        widgets = {
+            'name': forms.TextInput(attrs={'class': "form-control"}),
+            'display_name': forms.TextInput(attrs={'class': "form-control"}),
+            'description': forms.Textarea(attrs={'class': "form-control"}),
+            #'start_date': forms.DateTimeField(attrs={'class': "form-control"}),
+        }
 
 
 class ProblemEnvironmentCreateExecuteForm(forms.Form):
     pass
+
+
+class ProblemDescriptionUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Problem
+        fields = ('description', )
+        widgets = {
+            'description': forms.Textarea(attrs={'class': "form-control"}),
+        }

e5a0a1b52c344fea0a243a5d207de20ebdf815d9
tar-xzvff <jv120228m@gmail.com>
2018-05-10 20:07:43 +0900

    問題環境の作成とテストランのビューを追加

--- ictsc_prep_school/pstate/views/__init__.py
+++ ictsc_prep_school/pstate/views/__init__.py
@@ -244,8 +244,60 @@ class ProblemEnvironmentDetailView(LoginRequiredMixin, DetailView):
 
 class ProblemEnvironmentCreateView(LoginRequiredMixin, CreateView):
     form_class = ProblemEnvironmentForm
-    template_name = 'admin_pages/problem_environment/add.html'
+    template_name = 'admin_pages/common/add.html'
     success_url = "/manage/problem_environments/"
+
+    def form_valid(self, form):
+        print("デバッグ中です")
+        problem = Problem.objects.get(id=self.kwargs['pk'])
+        # workerに対して処理の実行命令.
+        from terraform_manager.models import Environment
+        environment = Environment(terraform_file=problem.terraform_file_id,
+                                  locked=False)
+        environment.save()
+
+        from terraform_manager.terraform_manager_tasks import direct_apply
+        var = []
+        direct_apply.delay(environment.id, problem.terraform_file_id.id, var)
+        # データの作成.
+        problem_environment = ProblemEnvironment(vnc_server_ipv4_address=None,
+                                                 is_enabled=True,
+                                                 team=form.cleaned_data['team'],
+                                                 participant=None,
+                                                 environment=environment,
+                                                 problem=problem)
+        problem_environment.save()
+        print(problem_environment.__dict__)
+        return HttpResponseRedirect(self.success_url)
+
+
+class ProblemEnvironmentTestRunExecuteView(LoginRequiredMixin, FormView):
+    template_name = 'admin_pages/problem/problem_environment_create_execute.html'
+    form_class = ProblemEnvironmentCreateExecuteForm
+    success_url = "/manage/problems/"
+
+    def form_valid(self, form):
+        problem = Problem.objects.get(id=self.kwargs['pk'])
+        # workerに対して処理の実行命令.
+        from terraform_manager.models import Environment
+        environment = Environment(terraform_file=problem.terraform_file_id,
+                                  locked=False)
+        environment.save()
+
+        from terraform_manager.terraform_manager_tasks import direct_apply
+        var = []
+        direct_apply.delay(environment.id, problem.terraform_file_id.id, var)
+
+        # データの作成.
+        problem_environment = ProblemEnvironment(vnc_server_ipv4_address=None,
+                                                 is_enabled=True,
+                                                 # teamもしくはparticipantが必ず指定される.
+                                                 team=None,
+                                                 participant=None,
+                                                 environment=environment,
+                                                 problem=problem)
+        problem_environment.save()
+        return HttpResponseRedirect(self.success_url)
 
 
 class ProblemEnvironmentUpdateView(UpdateView):

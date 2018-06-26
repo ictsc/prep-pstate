c542eb93e3f38ae97682b1cf724a9510e1f999ea
tar-xzvff <jv120228m@gmail.com>
2018-06-26 13:06:41 +0900

    開発環境用のsettingsにDBとMQの接続先情報を追加

--- ictsc_prep_school/ictsc_prep_school/settings/terraform_manager/develop.py
+++ ictsc_prep_school/ictsc_prep_school/settings/terraform_manager/develop.py
@@ -3,13 +3,15 @@ from ictsc_prep_school.settings.terraform_manager.base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'terraform_manager',
-        'USER': '',
+        'NAME': 'terraform_manager2',
+        'USER': 'terraform',
         'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'HOST': 'db.prep-dev.icttoracon.net',
+        'PORT': '5432',
     }
 }
 
-BROKER_URL = 'redis://:6379/0'
-CELERY_RESULT_BACKEND = 'redis://:6379/0'
+BROKER_URL = 'redis://59.106.211.157:6379/0'
+CELERY_RESULT_BACKEND = 'redis://59.106.211.157:6379/0'
+
+DEBUG = True

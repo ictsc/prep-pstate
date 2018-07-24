0e74455058519c6eea3f92f7d2f55a27d3084f14
tar-xzvff <jv120228m@gmail.com>
2018-07-24 15:06:30 +0900

    バージョン更新

--- docker_build.sh
+++ docker_build.sh
@@ -1,1 +1,1 @@
-eval $"docker build -t pstate_webui:20180722001 -f docker_build/pstate_webui/Dockerfile ."
+eval $"docker build -t pstate_webui:20180724002 -f docker_build/pstate_webui/Dockerfile ."

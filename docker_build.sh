59ca2f64d937335d3f5627b4dd7450b36b54095d
tar-xzvff <jv120228m@gmail.com>
2018-08-05 11:15:52 +0900

    Update app version

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t pstate_webui:20180804007 -f docker_build/pstate_webui/Dockerfile .
+docker build -t pstate_webui:20180804008 -f docker_build/pstate_webui/Dockerfile .
 docker build -t pstate-static-server:20180705 -f docker_build/pstate_webui_staticfiles_webserver/Dockerfile .
-docker build -t terraform-manager-worker:20180804006  -f docker_build/terraform_manager_worker/Dockerfile .
+docker build -t terraform-manager-worker:20180804008  -f docker_build/terraform_manager_worker/Dockerfile .

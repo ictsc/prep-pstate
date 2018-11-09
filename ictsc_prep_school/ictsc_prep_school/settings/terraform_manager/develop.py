f091f332d270beb8f8367d2d53e2bd405307a5fd
tar-xzvff <jv120228m@gmail.com>
2018-11-09 13:48:02 +0900

    開発環境で使用するMQのアドレスを変更

--- ictsc_prep_school/ictsc_prep_school/settings/terraform_manager/develop.py
+++ ictsc_prep_school/ictsc_prep_school/settings/terraform_manager/develop.py
@@ -11,7 +11,7 @@ DATABASES = {
     }
 }
 
-BROKER_URL = 'redis://59.106.211.157:6379/0'
-CELERY_RESULT_BACKEND = 'redis://59.106.211.157:6379/0'
+BROKER_URL = 'redis://redis.prep-dev.icttoracon.net:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis.prep-dev.icttoracon.net:6379/0'
 
 DEBUG = True

a10e4f971395d2aa5a57fab7be4d1d2fb43034d0
tar-xzvff <jv120228m@gmail.com>
2019-07-02 23:54:20 +0900

    docker-compose用の設定ファイルを追加

---
+++ ictsc_prep_school/ictsc_prep_school/settings/pstate/local.py
@@ -0,0 +1,36 @@
+from ictsc_prep_school.settings.pstate.base import *
+import os
+
+
+def get_env_variable(var_name, default=None):
+    """Get the environment variable or raise exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        if default is not None:
+            return default
+        else:
+            error_msg = 'The environment variable {} was missing, abort...' \
+                .format(var_name)
+            raise EnvironmentError(error_msg)
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '{}'.format(get_env_variable('POSTGRES_DB')),
+        'USER': '{}'.format(get_env_variable('POSTGRES_USER')),
+        'PASSWORD': '{}'.format(get_env_variable('POSTGRES_PASSWORD')),
+        'HOST': '{}'.format(get_env_variable('POSTGRES_HOST')),
+        'PORT': '{}'.format(get_env_variable('POSTGRES_PORT')),
+    }
+}
+
+BROKER_URL = 'redis://{}:{}/0'.format(get_env_variable('REDIS_HOST'), get_env_variable('REDIS_PORT'))
+CELERY_RESULT_BACKEND = 'redis://{}:{}/0'.format(get_env_variable('REDIS_HOST'), get_env_variable('REDIS_PORT'))
+
+CSRF_TRUSTED_ORIGINS = [
+    ".prep-dev.icttoracon.net",
+]
+
+DEBUG = True

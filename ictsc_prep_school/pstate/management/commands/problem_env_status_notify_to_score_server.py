018f0db65bd04acc15508baff9f78e2dd0963b31
tar-xzvff <jv120228m@gmail.com>
2019-07-17 10:58:44 +0900

    ログの出力設定を追加

--- ictsc_prep_school/pstate/management/commands/problem_env_status_notify_to_score_server.py
+++ ictsc_prep_school/pstate/management/commands/problem_env_status_notify_to_score_server.py
@@ -1,19 +1,10 @@
+import logging
+
 import requests
 from django.conf import settings
 from django.core.management import BaseCommand
 
-MUTATION_TEMPLATE_ADD_PROBLEM_ENV = '''
-mutation {
-    applyProblemEnvironment(input: {
-        problemCode: "{problem_code}", teamNumber: {team_number},
-        status: "{status}", host: "{host}",
-        user: "{vnc_login_username}", password: "{vnc_login_password}" }
-    ) {
-
-    errors
-  }
-}
-'''
+from pstate.models import NotificationQueue
 
 
 class Command(BaseCommand):
@@ -21,6 +12,7 @@ class Command(BaseCommand):
 
     def __init__(self):
         super().__init__()
+        self.logger = logging.getLogger('django.server')
         self.url = settings.SCORE_SERVER_API_URL
         self.auth_url = self.url + '/api/sessions'
         self.graphql_url = self.url + '/api/graphql'
@@ -30,7 +22,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.__login_score_server()
-        self.__request_to_score_server()
+        for notification in NotificationQueue.objects.all():
+            try:
+                if self.__request_to_score_server(notification.payload):
+                    notification.delete()
+            except:
+                pass
         self.__logout_score_server()
 
     def __login_score_server(self):
@@ -42,21 +39,11 @@ class Command(BaseCommand):
         response = self.session.delete(url=self.auth_url, timeout=5)
         response.raise_for_status()
 
-    def __request_to_score_server(self):
-        mutation = '''
-        mutation {
-            applyProblemEnvironment(input: {
-                problemCode: "AAA", teamNumber: 1,
-                status: "dummy status3", host: "192.168.0.1",
-                user: "ubuntu", password: "ubuntu" }
-            ) {
-            
-            errors
-          }
-        }
-        '''
+    def __request_to_score_server(self, mutation):
         data = {"query": mutation}
         response = self.session.post(url=self.graphql_url, data=data, timeout=5)
         response.raise_for_status()
-        if response.json()['data']['applyProblemEnvironment']['errors']:
-            raise Exception("API Error {}".format(response.content))
+        if 'errors' in response.json() and response.json()['errors'] != []:
+            self.logger.error("API Error {}, {}".format(response.content, mutation))
+            raise Exception("API Error")
+        return True

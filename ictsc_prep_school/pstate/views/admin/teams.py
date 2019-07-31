fc105542c0bad4ebf8bdbe0a334697f35b115419
sharknasuhorse <nakaryo6459shark@gmail.com>
2019-07-31 13:08:27 +0900

    リモートのリポジトリからCloneする処理の追加

--- ictsc_prep_school/pstate/views/admin/teams.py
+++ ictsc_prep_school/pstate/views/admin/teams.py
@@ -1,7 +1,7 @@
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView, FormView
 
 from pstate.forms.teams import TeamForm, TeamUpdateForm, TeamBulkCreateForm
-from pstate.models import Team
+from pstate.models import Team, Github
 from pstate.views.admin import LoginRequiredAndPermissionRequiredMixin
 
 
@@ -41,3 +41,26 @@ class TeamBulkCreateView(LoginRequiredAndPermissionRequiredMixin, FormView):
     form_class = TeamBulkCreateForm
     template_name = 'admin_pages/team/bulk-add.html'
     success_url = '/pstate/manage/teams/'
+
+    def form_valid(self, form):
+
+        # Githubの情報を読み込む
+        github = Github.objects.get(id=1)
+        git_source = github.git_source
+        ssh_private_key = github.ssh_private_key
+        project_root_path = github.project_root_path
+        teams_file = github.teams_file
+        
+        with open("key_file", mode="w") as f:
+            f.write(ssh_private_key)
+
+        import os
+        os.chmod("key_file", 0o600)
+
+        #リポジトリからClone
+        from git import Repo
+        ssh_cmd = 'ssh -i key_file'
+        Repo.clone_from("git@github.com:ictsc/ictsc-sandbox.git", "./github_clone", env={'GIT_SSH_COMMAND': ssh_cmd})
+
+
+        return HttpResponseRedirect(self.success_url)

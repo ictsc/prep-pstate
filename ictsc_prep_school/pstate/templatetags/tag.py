d7cd9c0ec80a080b61dcc3eb8f83641b3905c873
tar-xzvff <jv120228m@gmail.com>
2019-08-05 23:56:12 +0900

    Settingをtemplateから参照するための関数を追加

--- ictsc_prep_school/pstate/templatetags/tag.py
+++ ictsc_prep_school/pstate/templatetags/tag.py
@@ -1,8 +1,19 @@
 from django import template
 
+from terraform_manager.models import Setting
+
 register = template.Library()
 
 
 @register.filter
 def get_item(dictionary, key):
     return dictionary.getlist(key)
+
+
+@register.simple_tag
+def get_setting(key):
+    try:
+        #   FIX ME  :   Falseを返してもなぜかTrueになる.
+        return Setting.objects.get(name=key)
+    except:
+        return ''

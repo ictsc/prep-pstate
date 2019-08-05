a8178ffe8026a0e76679b2e7e6cc1c375102e899
sharknasuhorse <nakaryo6459shark@gmail.com>
2019-08-05 17:34:58 +0900

    start.shのshell_scriptを追加

--- ictsc_prep_school/pstate/views/admin/problems.py
+++ ictsc_prep_school/pstate/views/admin/problems.py
@@ -7,7 +7,7 @@ from pstate.models import Problem
 from pstate.views.admin import LoginRequiredAndPermissionRequiredMixin
 from django.http import HttpResponseRedirect
 from pstate.views.admin.github import GithubRepoPullExecute
-from terraform_manager.models import TerraformFile, Provider
+from terraform_manager.models import TerraformFile, Provider, ShellScript
 
 class ProblemCreateView(LoginRequiredAndPermissionRequiredMixin, CreateView):
     form_class = ProblemForm
@@ -79,6 +79,42 @@ class ProblemBulkCreateView(LoginRequiredAndPermissionRequiredMixin, FormView):
     success_url = '/pstate/manage/problems/'
 
     def form_valid(self, form):
+        start_sh_body = """
+## Change the password for ubuntu user
+echo "ubuntu:@@VNC_SERVER_PASSWORD@@" | chpasswd
+
+## disable fail2ban & network-manager
+service fail2ban stop
+chkconfig fail2ban off
+service network-manager stop
+chkconfig network-manager off
+
+## Interface setting
+echo auto eth1 >> /etc/network/interfaces
+echo iface eth1 inet static >> /etc/network/interfaces
+echo address 192.168.0.254 >> /etc/network/interfaces
+echo netmask 255.255.255.0 >> /etc/network/interfaces
+
+## VNC server settings
+echo @@VNC_SERVER_PASSWORD@@ | vncpasswd -f > "/home/ubuntu/.vnc/passwd"
+chmod 700 /home/ubuntu/.vnc/passwd
+chown ubuntu:ubuntu /home/ubuntu/.vnc/passwd
+
+systemctl daemon-reload
+systemctl start vncserver@\:1.service
+systemctl enable vncserver@\:1.service
+
+## NAPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth1 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+
+## NAPT permanent
+iptables-save > iptables.dat
+echo 'iptables-restore < /root/iptables.dat' >> /etc/rc.local
+
+service networking restart"""
+
         github = form.cleaned_data["github"]
         provider = form.cleaned_data["provider"]
         res = GithubRepoPullExecute(github)
@@ -86,8 +122,7 @@ class ProblemBulkCreateView(LoginRequiredAndPermissionRequiredMixin, FormView):
         #ex) ./github_clone/ictsc2019/q1
         problem_list_path = "./github_clone/%s/%s" % (github.project_root_path, github.problem_path) 
 
-        import glob, os
-        
+        import glob, os        
         problems = glob.glob(problem_list_path + "/**/")
 
         for problem in problems:
@@ -97,9 +132,10 @@ class ProblemBulkCreateView(LoginRequiredAndPermissionRequiredMixin, FormView):
                 problem_name = problem.split("/")[-2]
                 tf_file = TerraformFile(name="main", body=body, file_name="main.tf", provider=provider)
                 tf_file.save()
+                shell_script = ShellScript(name='VNCサーバ初期化用', file_name='start.sh', terraform_file=tf_file, body=start_sh_body)
+                shell_script.save()
 
                 problem = Problem(name=problem_name, display_name="test_display_name", terraform_file_id=tf_file)
                 problem.save()
 
         return HttpResponseRedirect(self.success_url)
-

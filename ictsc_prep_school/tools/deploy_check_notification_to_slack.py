dcdeaa77d1f6ee2ffbfa3a05f87a4db3c57ae06c
tar-xzvff <jv120228m@gmail.com>
2019-11-29 19:20:07 +0900

    通知内容を日本語に変更

--- ictsc_prep_school/tools/deploy_check_notification_to_slack.py
+++ ictsc_prep_school/tools/deploy_check_notification_to_slack.py
@@ -12,6 +12,19 @@ PSTATE_API_PASSWORD = ''
 
 WEB_HOOK_URL = ""
 
+BODY_TEMPLATE = """
+```実行待ち: {in_waiting}
+実行中: {in_applying}
+完了: {applied}
+失敗: {failed}
+削除中: {in_destroying}
+削除完了: {destroyed}
+
+進捗: {progress}%
+エラー: {error_rate}%
+```
+"""
+
 
 def get_problem_environment():
     response = requests.get(url=PSTATE_API_URL, auth=(PSTATE_API_USERNAME, PSTATE_API_PASSWORD), verify=False)
@@ -73,8 +86,19 @@ def show_problem_environment_status(problem_environment):
     print("-------------")
     print(buff)
     import json
+    d = {
+        "in_waiting": wait,
+        "in_applying": in_applying,
+        "applied": applied,
+        "failed": failed,
+        "in_destroying": in_destroying,
+        "destroyed": destroyed,
+        "progress": str(applied / total * 100),
+        "error_rate": str(failed / total * 100),
+         }
+    body = BODY_TEMPLATE.format(**d)
     requests.post(WEB_HOOK_URL, data=json.dumps({
-        'text': buff,  # 通知内容
+        'text': body,  # 通知内容
         'username': 'ictsc deploy bot',  # ユーザー名
         'icon_emoji': ':smile_cat:',  # アイコン
         'link_names': 1,  # 名前をリンク化
